fb0fbd08f6d223157339c285d3fa58a153d38275
naya <nastyasytnik6@gmail.com>
2026-06-15 15:12:23 +0300

    fix errorHandler

--- src/middleware/errorHandler.js
+++ src/middleware/errorHandler.js
@@ -1,10 +1,11 @@
-import createHttpError from 'http-errors';
+import { HttpError } from 'http-errors';
 
 export function errorHandler(err, req, res, next) {
-  const status =
-    err instanceof createHttpError.HttpError ? err.status : err.status || 500;
+  const isHttpError = err instanceof HttpError;
+
+  const status = isHttpError ? err.status : 500;
 
   res.status(status).json({
-    message: err.message,
+    message: err.message || 'Internal Server Error',
   });
 }
