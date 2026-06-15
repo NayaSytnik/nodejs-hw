d01a6d197d2f3146b85df6520f6c6832ea35babf
naya <nastyasytnik6@gmail.com>
2026-06-15 15:03:23 +0300

    fix

--- src/middleware/errorHandler.js
+++ src/middleware/errorHandler.js
@@ -1,5 +1,8 @@
-export default function errorHandler(err, req, res, next) {
-  const status = err.status || 500;
+import createHttpError from 'http-errors';
+
+export function errorHandler(err, req, res, next) {
+  const status =
+    err instanceof createHttpError.HttpError ? err.status : err.status || 500;
 
   res.status(status).json({
     message: err.message,
--- src/server.js
+++ src/server.js
@@ -3,9 +3,9 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 
 import { connectMongoDB } from './db/connectMongoDB.js';
-import logger from './middleware/logger.js';
-import notFoundHandler from './middleware/notFoundHandler.js';
-import errorHandler from './middleware/errorHandler.js';
+import { logger } from './middleware/logger.js';
+import { notFoundHandler } from './middleware/notFoundHandler.js';
+import { errorHandler } from './middleware/errorHandler.js';
 import notesRouter from './routes/notesRoutes.js';
 
 dotenv.config();
